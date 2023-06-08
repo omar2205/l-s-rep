bc62e2395f085c84ca49bb279e98a784f53851f9
omar2205 <omar2205a@yahoo.com>
2023-06-08 11:07:52 +0300

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Delete from "./Delete.svelte";
+</script>
+
+<main>
+  <h1>Lorem ipsum dolor sit amet.</h1>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse non similique
+    sapiente illum recusandae natus?
+  </p>
+
+  <div class="flex gap-4 ml-auto mb-2">
+    <Delete />
+  </div>
+</main>
